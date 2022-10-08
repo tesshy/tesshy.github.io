d24ceb7030202dc7fd3d490f5757662a6bdea616
Tetsushi Watanabe <tesshy@users.noreply.github.com>
2022-10-08 16:39:54 +0900

    buildno

--- conf.py
+++ conf.py
@@ -646,7 +646,7 @@ GITHUB_COMMIT_SOURCE = True
 # Where the output site should be located
 # If you don't use an absolute path, it will be considered as relative
 # to the location of conf.py
-OUTPUT_FOLDER = 'docs'
+OUTPUT_FOLDER = '_build'
 
 # where the "cache" of partial generated content should be located
 # default: 'cache'
